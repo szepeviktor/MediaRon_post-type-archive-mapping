daada79d36270402a1aac13abf7a8a846543a8d3
Ronald Huereca <rhuereca@updraftplus.com>
2020-02-03 10:06:05 -0600

    Adding SVG to loading screen.

--- src/block/edit.js
+++ src/block/edit.js
@@ -899,7 +899,7 @@ class PTAM_Custom_Posts extends Component {
 					{ inspectorControls }
 					<Placeholder>
 						<div className="ptam-loading">
-							<h1>{__( 'Custom Posts', 'post-type-archive-mapping' )}</h1>
+							<h1><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 14h4v-4H4v4zm0 5h4v-4H4v4zM4 9h4V5H4v4zm5 5h12v-4H9v4zm0 5h12v-4H9v4zM9 5v4h12V5H9z"/><path d="M0 0h24v24H0z" fill="none"/></svg> {__( 'Custom Posts', 'post-type-archive-mapping' )}</h1>
 							<h2>{__( 'Finding items...', 'post-type-archive-mapping' )} <Spinner /></h2>
 						</div>
 					</Placeholder>
