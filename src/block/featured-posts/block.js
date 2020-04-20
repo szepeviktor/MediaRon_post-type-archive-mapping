b47ad65c6cd14ed0b744eaa65a303c5c83282495
Ronald Huereca <rhuereca@updraftplus.com>
2020-04-19 23:21:43 -0500

    Adding block descriptions.

--- src/block/featured-posts/block.js
+++ src/block/featured-posts/block.js
@@ -38,6 +38,7 @@ registerBlockType( 'ptam/featured-posts', { // Block name. Block names must be s
 	title: __( 'Featured Posts by Term', 'post-type-archive-mapping' ), // Block title.
 	icon: <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-9 8H3V9h9v2zm0-4H3V5h9v2z"/></svg>,
 	category: 'ptam-custom-query-blocks', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	description: __('Highlight a term (category) and show the items within it. Perfect for term archives.', 'post-type-archive-mapping'),
 	keywords: [
 		__( 'featured', 'post-type-archive-mapping' ),
 		__( 'post', 'poost-type-archive-mapping' ),
