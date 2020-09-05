32974473cb4c1e7d5b5ceb1d5d3ccccbb25389e1
Ronald Huereca <rhuereca@updraftplus.com>
2020-09-05 14:22:51 -0500

    Updating style dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	mode: process.env.NODE_ENV,
+	entry: {
+		blocks: ["./src/blocks.js"],
+		blockstyles: ["./src/block/style.scss"],
+	},
+	output: {
+		filename: "[name].js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "fonts/",
+							esModule: false,
+						},
+					},
+				],
+      },
+      {
+				test: /\.(png|jpg|gif)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "images/",
+							esModule: false,
+						},
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true,
+						},
+					},
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: "babel-loader",
+				options: { presets: ["@babel/env"] },
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+		}),
+	],
+};
