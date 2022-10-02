9d59261db0a7ddad96cbc79d6b3eb8dc320db7cb
Ronald Huereca <ronald@mediaron.com>
2022-10-02 00:57:23 -0500

    Doing dev build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '6a9711b8c8007aad574f');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'd6c096bf6e3c2a65a3de');
